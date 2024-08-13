be174b4828d5acc1b5cda072c435f67d31b38fb9
Zach Lewis <zaclewis@gmail.com>
2024-08-13 13:52:50 -0500

    works

---
+++ openlayers/demo/main.js
@@ -0,0 +1,32 @@
+import Graticule from 'ol/layer/Graticule.js';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import Stroke from 'ol/style/Stroke.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+import {fromLonLat} from 'ol/proj.js';
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM({
+        wrapX: false,
+      }),
+    }),
+    new Graticule({
+      // the style to use for the lines, optional.
+      strokeStyle: new Stroke({
+        color: 'rgba(255,120,0,0.9)',
+        width: 2,
+        lineDash: [0.5, 4],
+      }),
+      showLabels: true,
+      wrapX: false,
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    center: fromLonLat([4.8, 47.75]),
+    zoom: 5,
+  }),
+});
