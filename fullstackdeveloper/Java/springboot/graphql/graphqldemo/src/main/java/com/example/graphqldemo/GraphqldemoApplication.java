1e155cd1517b3c35dfb30bc449bdde1069068d48
zach <zaclewis@gmail.com>
2024-10-03 16:10:17 -0500

    allow all

--- fullstackdeveloper/Java/springboot/graphql/graphqldemo/src/main/java/com/example/graphqldemo/GraphqldemoApplication.java
+++ fullstackdeveloper/Java/springboot/graphql/graphqldemo/src/main/java/com/example/graphqldemo/GraphqldemoApplication.java
@@ -9,9 +9,27 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class GraphqldemoApplication {
+
+    
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqldemoApplication.class, args);
@@ -66,4 +84,7 @@ class BookController {
         return Book.getById(id);
     }
 
+
+
 }
+
