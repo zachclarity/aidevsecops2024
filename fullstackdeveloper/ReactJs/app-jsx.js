1052f62c49f1c03972cedeaf52006dc62d669c7d
zach <zaclewis@gmail.com>
2024-10-22 14:40:30 -0500

    d

---
+++ fullstackdeveloper/ReactJs/app-jsx.js
@@ -0,0 +1,11 @@
+import CameraApp from './components/CameraApp'
+
+function App() {
+  return (
+    <main className="min-h-screen bg-slate-50 p-4">
+      <CameraApp />
+    </main>
+  )
+}
+
+export default App
