e4fd01a229e4c0ecd4eda25e342452dfc1ee24d8
Zac Lewis <info@zactonics.ai>
2025-05-22 09:58:54 -0500

    add docs

--- fullstackdeveloper/Java/springboot/graphql/graphqldemo/src/main/java/com/example/graphqldemo/GraphqldemoApplication.java
+++ fullstackdeveloper/Java/springboot/graphql/graphqldemo/src/main/java/com/example/graphqldemo/GraphqldemoApplication.java
@@ -8,10 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.graphql.server.webmvc.GraphQlWebSocketHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
 public class GraphqldemoApplication {
@@ -59,20 +64,20 @@ query MyQuery {
   }
 }
 
-curl "http://localhost:9999/graphql" ^
-  -H "Accept-Language: en-US,en;q=0.9" ^
-  -H "Connection: keep-alive" ^
-  -H "Origin: http://localhost:9999" ^
-  -H "Referer: http://localhost:9999/graphiql?path=/graphql" ^
-  -H "Sec-Fetch-Dest: empty" ^
-  -H "Sec-Fetch-Mode: cors" ^
-  -H "Sec-Fetch-Site: same-origin" ^
-  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" ^
-  -H "accept: application/json, multipart/mixed" ^
-  -H "content-type: application/json" ^
-  -H ^"sec-ch-ua: ^\^"Chromium^\^";v=^\^"122^\^", ^\^"Not(A:Brand^\^";v=^\^"24^\^", ^\^"Google Chrome^\^";v=^\^"122^\^"^" ^
-  -H "sec-ch-ua-mobile: ?0" ^
-  -H ^"sec-ch-ua-platform: ^\^"Windows^\^"^" ^
+curl "http://localhost:9999/graphql" \
+  -H "Accept-Language: en-US,en;q=0.9" \
+  -H "Connection: keep-alive" \
+  -H "Origin: http://localhost:9999" \
+  -H "Referer: http://localhost:9999/graphiql?path=/graphql" \
+  -H "Sec-Fetch-Dest: empty" \
+  -H "Sec-Fetch-Mode: cors" \
+  -H "Sec-Fetch-Site: same-origin" \
+  -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36" \
+  -H "accept: application/json, multipart/mixed" \
+  -H "content-type: application/json" \
+  -H ^"sec-ch-ua: ^\^"Chromium^\^";v=^\^"122^\^", ^\^"Not(A:Brand^\^";v=^\^"24^\^", ^\^"Google Chrome^\^";v=^\^"122^\^"^" \
+  -H "sec-ch-ua-mobile: ?0" \
+  -H ^"sec-ch-ua-platform: ^\^"Windows^\^"^" \
   --data-raw ^"^{^\^"query^\^":^\^"query MyQuery ^{^\^\n  bookById(id: ^\^\^\^"book-1^\^\^\^") ^{^\^\n    name^\^\n  ^}^\^\n^}^\^",^\^"operationName^\^":^\^"MyQuery^\^"^}^"
  * 
  */
@@ -84,7 +89,16 @@ class BookController {
         return Book.getById(id);
     }
 
+/* *
 
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new GraphQlWebSocketHandler(null, null, null), "/subscriptions").setAllowedOrigins("*");
+    }
+}*/
 
 }
 
